43a962323fffd5dd7f763b0bb423ded180179609
cy5211 <501291364@qq.com>
2019-01-11 11:21:23 +0800

    2

--- datasets/download_and_convert_PetFinder.py
+++ datasets/download_and_convert_PetFinder.py
@@ -12,127 +12,142 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-r"""Downloads and converts cifar10 data to TFRecords of TF-Example protos.
+r"""Downloads and converts Flowers data to TFRecords of TF-Example protos.
 
-This module downloads the cifar10 data, uncompresses it, reads the files
-that make up the cifar10 data and creates two TFRecord datasets: one for train
+This module downloads the Flowers data, uncompresses it, reads the files
+that make up the Flowers data and creates two TFRecord datasets: one for train
 and one for test. Each TFRecord dataset is comprised of a set of TF-Example
 protocol buffers, each of which contain a single image and label.
 
-The script should take several minutes to run.
+The script should take about a minute to run.
 
 """
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import math
 import os
+import random
 import sys
-import tarfile
 
-import numpy as np
-from six.moves import cPickle
-from six.moves import urllib
 import tensorflow as tf
 
 from datasets import dataset_utils
 
+# The URL where the Flowers data can be downloaded.
+_DATA_URL = 'http://download.tensorflow.org/example_images/flower_photos.tgz'
 
-# The number of training files.
-_NUM_TRAIN_FILES = 5
+# The number of images in the validation set.
+_NUM_VALIDATION = 9718
 
-# The height and width of each image.
-_IMAGE_SIZE = 224
+# Seed for repeatability.
+_RANDOM_SEED = 0
 
-# The names of the classes.
-_CLASS_NAMES = [
-    '0',
-    '1',
-    '2',
-    '3',
-    '4',
-]
+# The number of shards per dataset split.
+_NUM_SHARDS = 5
 
 
-def _add_to_tfrecord(filename, tfrecord_writer, offset=0):
-  """Loads data from the cifar10 pickle files and writes files to a TFRecord.
+class ImageReader(object):
+  """Helper class that provides TensorFlow image coding utilities."""
+
+  def __init__(self):
+    # Initializes function that decodes RGB JPEG data.
+    self._decode_jpeg_data = tf.placeholder(dtype=tf.string)
+    self._decode_jpeg = tf.image.decode_jpeg(self._decode_jpeg_data, channels=3)
+
+  def read_image_dims(self, sess, image_data):
+    image = self.decode_jpeg(sess, image_data)
+    return image.shape[0], image.shape[1]
+
+  def decode_jpeg(self, sess, image_data):
+    image = sess.run(self._decode_jpeg,
+                     feed_dict={self._decode_jpeg_data: image_data})
+    assert len(image.shape) == 3
+    assert image.shape[2] == 3
+    return image
+
+
+def _get_filenames_and_classes(dataset_dir):
+  """Returns a list of filenames and inferred class names.
 
   Args:
-    filename: The filename of the cifar10 pickle file.
-    tfrecord_writer: The TFRecord writer to use for writing.
-    offset: An offset into the absolute number of images previously written.
+    dataset_dir: A directory containing a set of subdirectories representing
+      class names. Each subdirectory should contain PNG or JPG encoded images.
 
   Returns:
-    The new offset.
+    A list of image file paths, relative to `dataset_dir` and the list of
+    subdirectories, representing class names.
   """
-  with tf.gfile.Open(filename, 'rb') as f:
-    if sys.version_info < (3,):
-      data = cPickle.load(f)
-    else:
-      data = cPickle.load(f, encoding='bytes')
+  flower_root = os.path.join(dataset_dir, 'flower_photos')
+  directories = []
+  class_names = []
+  for filename in os.listdir(flower_root):
+    path = os.path.join(flower_root, filename)
+    if os.path.isdir(path):
+      directories.append(path)
+      class_names.append(filename)
 
-  images = data[b'data']
-  num_images = images.shape[0]
+  photo_filenames = []
+  for directory in directories:
+    for filename in os.listdir(directory):
+      path = os.path.join(directory, filename)
+      photo_filenames.append(path)
 
-  images = images.reshape((num_images, 3, _IMAGE_SIZE, _IMAGE_SIZE))
-  labels = data[b'labels']
+  return photo_filenames, sorted(class_names)
+
+
+def _get_dataset_filename(dataset_dir, split_name, shard_id):
+  output_filename = 'PetFinder_%s_%05d-of-%05d.tfrecord' % (
+      split_name, shard_id, _NUM_SHARDS)
+  return os.path.join(dataset_dir, output_filename)
+
+
+def _convert_dataset(split_name, filenames, class_names_to_ids, dataset_dir):
+  """Converts the given filenames to a TFRecord dataset.
+
+  Args:
+    split_name: The name of the dataset, either 'train' or 'validation'.
+    filenames: A list of absolute paths to png or jpg images.
+    class_names_to_ids: A dictionary from class names (strings) to ids
+      (integers).
+    dataset_dir: The directory where the converted datasets are stored.
+  """
+  assert split_name in ['train', 'validation']
+
+  num_per_shard = int(math.ceil(len(filenames) / float(_NUM_SHARDS)))
 
   with tf.Graph().as_default():
-    image_placeholder = tf.placeholder(dtype=tf.uint8)
-    encoded_image = tf.image.encode_png(image_placeholder)
+    image_reader = ImageReader()
 
     with tf.Session('') as sess:
 
-      for j in range(num_images):
-        sys.stdout.write('\r>> Reading file [%s] image %d/%d' % (
-            filename, offset + j + 1, offset + num_images))
-        sys.stdout.flush()
+      for shard_id in range(_NUM_SHARDS):
+        output_filename = _get_dataset_filename(
+            dataset_dir, split_name, shard_id)
 
-        image = np.squeeze(images[j]).transpose((1, 2, 0))
-        label = labels[j]
+        with tf.python_io.TFRecordWriter(output_filename) as tfrecord_writer:
+          start_ndx = shard_id * num_per_shard
+          end_ndx = min((shard_id+1) * num_per_shard, len(filenames))
+          for i in range(start_ndx, end_ndx):
+            sys.stdout.write('\r>> Converting image %d/%d shard %d' % (
+                i+1, len(filenames), shard_id))
+            sys.stdout.flush()
 
-        png_string = sess.run(encoded_image,
-                              feed_dict={image_placeholder: image})
+            # Read the filename:
+            image_data = tf.gfile.FastGFile(filenames[i], 'rb').read()
+            height, width = image_reader.read_image_dims(sess, image_data)
 
-        example = dataset_utils.image_to_tfexample(
-            png_string, b'png', _IMAGE_SIZE, _IMAGE_SIZE, label)
-        tfrecord_writer.write(example.SerializeToString())
+            class_name = os.path.basename(os.path.dirname(filenames[i]))
+            class_id = class_names_to_ids[class_name]
 
-  return offset + num_images
+            example = dataset_utils.image_to_tfexample(
+                image_data, b'jpg', height, width, class_id)
+            tfrecord_writer.write(example.SerializeToString())
 
-
-def _get_output_filename(dataset_dir, split_name):
-  """Creates the output filename.
-
-  Args:
-    dataset_dir: The dataset directory where the dataset is stored.
-    split_name: The name of the train/test split.
-
-  Returns:
-    An absolute file path.
-  """
-  return '%s/cifar10_%s.tfrecord' % (dataset_dir, split_name)
-
-
-def _download_and_uncompress_dataset(dataset_dir):
-  """Downloads cifar10 and uncompresses it locally.
-
-  Args:
-    dataset_dir: The directory where the temporary files are stored.
-  """
-  filename = _DATA_URL.split('/')[-1]
-  filepath = os.path.join(dataset_dir, filename)
-
-  if not os.path.exists(filepath):
-    def _progress(count, block_size, total_size):
-      sys.stdout.write('\r>> Downloading %s %.1f%%' % (
-          filename, float(count * block_size) / float(total_size) * 100.0))
-      sys.stdout.flush()
-    filepath, _ = urllib.request.urlretrieve(_DATA_URL, filepath, _progress)
-    print()
-    statinfo = os.stat(filepath)
-    print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
-    tarfile.open(filepath, 'r:gz').extractall(dataset_dir)
+  sys.stdout.write('\n')
+  sys.stdout.flush()
 
 
 def _clean_up_temporary_files(dataset_dir):
@@ -145,8 +160,18 @@ def _clean_up_temporary_files(dataset_dir):
   filepath = os.path.join(dataset_dir, filename)
   tf.gfile.Remove(filepath)
 
-  tmp_dir = os.path.join(dataset_dir, 'cifar-10-batches-py')
+  tmp_dir = os.path.join(dataset_dir, 'flower_photos')
   tf.gfile.DeleteRecursively(tmp_dir)
+
+
+def _dataset_exists(dataset_dir):
+  for split_name in ['train', 'validation']:
+    for shard_id in range(_NUM_SHARDS):
+      output_filename = _get_dataset_filename(
+          dataset_dir, split_name, shard_id)
+      if not tf.gfile.Exists(output_filename):
+        return False
+  return True
 
 
 def run(dataset_dir):
@@ -155,36 +180,30 @@ def run(dataset_dir):
   Args:
     dataset_dir: The dataset directory where the dataset is stored.
   """
-  
   if not tf.gfile.Exists(dataset_dir):
     tf.gfile.MakeDirs(dataset_dir)
 
-  training_filename = _get_output_filename(dataset_dir, 'train')
-  testing_filename = _get_output_filename(dataset_dir, 'test')
-
-  if tf.gfile.Exists(training_filename) and tf.gfile.Exists(testing_filename):
+  if _dataset_exists(dataset_dir):
     print('Dataset files already exist. Exiting without re-creating them.')
     return
 
+  photo_filenames, class_names = _get_filenames_and_classes(dataset_dir)
+  class_names_to_ids = dict(zip(class_names, range(len(class_names))))
 
-  # First, process the training data:
-  with tf.python_io.TFRecordWriter(training_filename) as tfrecord_writer:
-    offset = 0
-    for i in range(_NUM_TRAIN_FILES):
-      filename = os.path.join(dataset_dir,
-                              'PetFinder-batches-py',
-                              'data_batch_%d' % (i + 1))  # 1-indexed.
-      offset = _add_to_tfrecord(filename, tfrecord_writer, offset)
+  # Divide into train and test:
+  random.seed(_RANDOM_SEED)
+  random.shuffle(photo_filenames)
+  training_filenames = photo_filenames[_NUM_VALIDATION:]
+  validation_filenames = photo_filenames[:_NUM_VALIDATION]
 
-  # Next, process the testing data:
-  with tf.python_io.TFRecordWriter(testing_filename) as tfrecord_writer:
-    filename = os.path.join(dataset_dir,
-                            'PetFinder-batches-py',
-                            'test_batch')
-    _add_to_tfrecord(filename, tfrecord_writer)
+  # First, convert the training and validation sets.
+  _convert_dataset('train', training_filenames, class_names_to_ids,
+                   dataset_dir)
+  _convert_dataset('validation', validation_filenames, class_names_to_ids,
+                   dataset_dir)
 
   # Finally, write the labels file:
-  labels_to_class_names = dict(zip(range(len(_CLASS_NAMES)), _CLASS_NAMES))
+  labels_to_class_names = dict(zip(range(len(class_names)), class_names))
   dataset_utils.write_label_file(labels_to_class_names, dataset_dir)
 
   # _clean_up_temporary_files(dataset_dir)
